26e4948e3bececd8746fbec2348fc0918af67abb
emptyhopes <literallymyfault@gmail.com>
2023-11-29 17:57:47 +0500

    refactoring

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
 /*
 	=== Утилита sort ===
 
@@ -26,5 +34,46 @@ package main
 */
 
 func main() {
+	//key := flag.String("k", "", "")
+	//numeric := flag.String("n", "", "")
+	//reverse := flag.String("r", "", "")
+	//unique := flag.String("u", "", "")
+	//
+	//month := flag.String("M", "", "")
+	//ignore := flag.String("b", "", "")
+	//check := flag.String("c", "", "")
+	//suffix := flag.String("h", "", "")
 
+	flag.Parse()
+
+	pwd, _ := os.Getwd()
+
+	file := flag.Args()[0]
+	filepath := path.Join(pwd, file)
+
+	fmt.Println(filepath)
+
+	openFile, err := os.OpenFile(filepath, os.O_RDWR, 0755)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+
+		return
+	}
+
+	defer openFile.Close()
+
+	data := make([]byte, 0, 100)
+
+	n, err := openFile.Read(data)
+
+	fmt.Println(n)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+
+		return
+	}
+
+	fmt.Println(data)
 }
