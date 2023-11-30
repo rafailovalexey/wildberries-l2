6c59664196d8d353ec3622502ce723d8dbda3d57
emptyhopes <literallymyfault@gmail.com>
2023-11-30 20:28:13 +0500

    dev01, dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 /*
 	=== Утилита cut ===
 
@@ -13,6 +21,84 @@ package main
 	Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type FlagsInterface interface {
+	InitializeFlags()
+}
 
+type Flags struct {
+	Fields    string
+	Delimiter string
+	Separated bool
+}
+
+var _ FlagsInterface = (*Flags)(nil)
+
+func (f *Flags) InitializeFlags() {
+	fields := flag.String("f", "", "")
+	delimiter := flag.String("d", "", "")
+	separated := flag.Bool("s", false, "")
+
+	flag.Parse()
+
+	f.Fields = *fields
+	f.Delimiter = *delimiter
+	f.Separated = *separated
+}
+
+type ApplicationInterface interface{}
+
+type Application struct{}
+
+var _ ApplicationInterface = (*Application)(nil)
+
+func main() {
+	flags := &Flags{}
+
+	flags.InitializeFlags()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if flags.Separated && !strings.Contains(line, flags.Delimiter) {
+			continue
+		}
+
+		fieldsList := strings.Split(line, flags.Delimiter)
+
+		var selectedFields []string
+
+		if flags.Fields != "" {
+			fieldIndices := strings.Split(flags.Fields, ",")
+
+			for _, indexStr := range fieldIndices {
+				index := parseInt(indexStr)
+
+				if index > 0 && index <= len(fieldsList) {
+					selectedFields = append(selectedFields, fieldsList[index-1])
+				}
+			}
+		} else {
+			selectedFields = fieldsList
+		}
+
+		fmt.Println(strings.Join(selectedFields, flags.Delimiter))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "ошибка чтения стандартного ввода:", err)
+	}
+}
+
+func parseInt(s string) int {
+	var result int
+
+	_, err := fmt.Sscanf(s, "%d", &result)
+
+	if err != nil {
+		return 0
+	}
+
+	return result
 }
