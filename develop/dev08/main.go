9ae53fbe221a15eb320d2cc533e7bd242b743d7c
emptyhopes <literallymyfault@gmail.com>
2023-12-01 11:22:02 +0500

    dev08

--- develop/dev08/main.go
+++ develop/dev08/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 	=== Взаимодействие с ОС ===
 
@@ -14,6 +23,157 @@ package main
 	Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Command struct {
+	name   string
+	handle func([]string) (bool, error)
+}
 
+type Commands = []Command
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		commands := GetCommands()
+
+		fmt.Printf("$ ")
+
+		scanner.Scan()
+
+		input := scanner.Text()
+
+		arguments := strings.Fields(input)
+
+		if command, isExist := commands[arguments[0]]; isExist {
+			exit, err := command.handle(arguments)
+
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			}
+
+			if exit {
+				break
+			}
+		}
+
+		if _, isExist := commands[arguments[0]]; !isExist {
+			exit, err := commands["default"].handle(arguments)
+
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			}
+
+			if exit {
+				break
+			}
+		}
+	}
+}
+
+func GetCommands() map[string]Command {
+	commands := &Commands{
+		Command{
+			name: "cd",
+			handle: func(arguments []string) (bool, error) {
+				if len(arguments) < 2 {
+					return false, errors.New("использование: cd <directory>")
+				}
+
+				err := os.Chdir(arguments[1])
+
+				if err != nil {
+					return false, errors.New(fmt.Sprintf("ошибка при изменении директории %v", err))
+				}
+
+				return false, nil
+			},
+		},
+		Command{
+			name: "pwd",
+			handle: func(arguments []string) (bool, error) {
+				directory, err := os.Getwd()
+
+				if err != nil {
+					return false, errors.New(fmt.Sprintf("ошибка при получении текущей директории %v", err))
+				}
+
+				fmt.Printf("%s\n", directory)
+
+				return false, nil
+			},
+		},
+		Command{
+			name: "echo",
+			handle: func(arguments []string) (bool, error) {
+				fmt.Printf("%s\n", strings.Join(arguments[1:], " "))
+
+				return false, nil
+			},
+		},
+		Command{
+			name: "kill",
+			handle: func(arguments []string) (bool, error) {
+				if len(arguments) < 2 {
+					return false, errors.New(fmt.Sprintf("использование: kill <pid>"))
+				}
+
+				cmd := exec.Command("kill", arguments[1:]...)
+
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+
+				if err := cmd.Run(); err != nil {
+					return false, errors.New(fmt.Sprintf("ошибка выполнения команды %v", err))
+				}
+
+				return false, nil
+			},
+		},
+		Command{
+			name: "ps",
+			handle: func(arguments []string) (bool, error) {
+				cmd := exec.Command("ps")
+
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+
+				if err := cmd.Run(); err != nil {
+					return false, errors.New(fmt.Sprintf("ошибка выполнения команды %v", err))
+				}
+
+				return false, nil
+			},
+		},
+		Command{
+			name: "exit",
+			handle: func(arguments []string) (bool, error) {
+				fmt.Printf("выход из терминала\n")
+
+				return true, nil
+			},
+		},
+		Command{
+			name: "default",
+			handle: func(arguments []string) (bool, error) {
+				cmd := exec.Command(arguments[0], arguments[1:]...)
+
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+
+				if err := cmd.Run(); err != nil {
+					return false, errors.New(fmt.Sprintf("ошибка выполнения команды %v", err))
+				}
+
+				return false, nil
+			},
+		},
+	}
+
+	dictionary := make(map[string]Command, len(*commands))
+
+	for _, command := range *commands {
+		dictionary[command.name] = command
+	}
+
+	return dictionary
 }
