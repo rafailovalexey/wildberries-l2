d65f4306810f5420d765071f2e3c8382c2384b27
emptyhopes <literallymyfault@gmail.com>
2023-12-01 17:16:05 +0500

    dev10

--- develop/dev10/main.go
+++ develop/dev10/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
 /*
 	=== Утилита telnet ===
 
@@ -15,6 +24,107 @@ package main
 	При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+type FlagsInterface interface {
+	InitializeFlags()
+}
 
+type Flags struct {
+	Timeout time.Duration
+}
+
+var _ FlagsInterface = (*Flags)(nil)
+
+func (f *Flags) InitializeFlags() {
+	timeout := flag.Duration("timeout", 10*time.Second, "timeout for connection")
+
+	flag.Parse()
+
+	f.Timeout = *timeout
+}
+
+type ApplicationInterface interface {
+	Connection(address string, flags *Flags)
+}
+
+type Application struct{}
+
+var _ ApplicationInterface = (*Application)(nil)
+
+func main() {
+	flags := &Flags{}
+	application := &Application{}
+
+	flags.InitializeFlags()
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	if host == "" || port == "" {
+		fmt.Printf("usage: [--timeout=<timeout>] <host> <port>\n")
+
+		os.Exit(1)
+	}
+
+	address := fmt.Sprintf("%s:%s", host, port)
+
+	application.Connection(address, flags)
+}
+
+func (a *Application) Connection(address string, flags *Flags) {
+	connection, err := net.DialTimeout("tcp", address, flags.Timeout)
+
+	if err != nil {
+		fmt.Printf("error connecting to the server %v\n", err)
+
+		os.Exit(1)
+	}
+
+	defer connection.Close()
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt)
+
+	go func() {
+		buffer := make([]byte, 1024)
+
+		for {
+			length, err := connection.Read(buffer)
+
+			if err != nil {
+				fmt.Printf("error reading from the server %v\n", err)
+
+				break
+			}
+
+			fmt.Print(string(buffer[:length]))
+		}
+	}()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		length, err := os.Stdin.Read(buffer)
+
+		if err != nil {
+			fmt.Printf("error reading from STDIN %v\n", err)
+
+			break
+		}
+
+		_, err = connection.Write(buffer[:length])
+
+		if err != nil {
+			fmt.Printf("error writing to the server %v\n", err)
+
+			break
+		}
+	}
+
+	select {
+	case <-done:
+		fmt.Printf("connection closed by user\n")
+	case <-time.After(flags.Timeout):
+		fmt.Printf("timeout reached, connection closed\n")
+	}
 }
