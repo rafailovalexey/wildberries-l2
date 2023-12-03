9082cc65a5770830ff91ba2bad9a35471a1fb1c3
emptyhopes <literallymyfault@gmail.com>
2023-12-03 09:46:34 +0500

    dev102 test

--- develop/dev02/main_test.go
+++ develop/dev02/main_test.go
@@ -1,1 +1,32 @@
 package main
+
+import "testing"
+
+type Test struct {
+	input    string
+	expected string
+}
+
+type Tests = []Test
+
+func TestUnpackString(t *testing.T) {
+	tests := &Tests{
+		{"a4bc2d5e", "aaaabccddddde"},
+		{"abcd", "abcd"},
+		{"45", ""},
+		{"", ""},
+		{"qwe\\4\\5", "qwe45"},
+		{"qwe\\45", "qwe44444"},
+		{"qwe\\\\5", "qwe\\\\\\\\\\"},
+	}
+
+	application := &Application{}
+
+	for _, test := range *tests {
+		result := application.UnpackString(test.input)
+
+		if result != test.expected {
+			t.Errorf("Input: %s, Expected: %s, Result: %s", test.input, test.expected, result)
+		}
+	}
+}
