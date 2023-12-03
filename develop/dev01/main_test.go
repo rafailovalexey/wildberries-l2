265d928cb020e38b673296d9201bb7efd0be056e
emptyhopes <literallymyfault@gmail.com>
2023-12-03 13:34:41 +0500

    dev01 tests dev04 tests

--- develop/dev01/main_test.go
+++ develop/dev01/main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/beevik/ntp"
+	"testing"
+	"time"
+)
+
+func TestNtpTime(t *testing.T) {
+	application := &Application{}
+
+	ntpTime, err := application.GetTimeNtp()
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	mockNtpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if ntpTime.Format(time.RFC822) != mockNtpTime.Format(time.RFC822) {
+		t.Errorf("got %s expected %s", ntpTime.Format(time.RFC822), mockNtpTime.Format(time.RFC822))
+	}
+}
--- develop/dev04/main_test.go
+++ develop/dev04/main_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Test struct {
+	words    []string
+	expected map[string][]string
+}
+
+type Tests = []Test
+
+func TestFindAnagrams(t *testing.T) {
+	application := &Application{}
+
+	tests := Tests{
+		{
+			words:    []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик"},
+			expected: map[string][]string{"акптя": {"пятак", "пятка", "тяпка"}, "иклост": {"листок", "слиток", "столик"}},
+		},
+		{
+			words:    []string{"мама", "амам", "тот", "тот"},
+			expected: map[string][]string{"аамм": {"амам", "мама"}, "отт": {"тот", "тот"}},
+		},
+	}
+
+	for _, test := range tests {
+		result := application.FindAnagrams(test.words)
+
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("for words %v expected %v but got %v", test.words, test.expected, result)
+		}
+	}
+}
